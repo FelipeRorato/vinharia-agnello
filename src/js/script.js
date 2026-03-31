9f6185efa73552a5ebf5d815ee6e12c88d94479e
Felipe <feliperoratocosta1@gmail.com>
2026-03-31 18:13:41 -0300

    feat/Felipe: 1a versao do site com o script.js

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+let nome = prompt("Digite o nome do seu vinho: ");
+let tipo = prompt("Digite o tipo do seu vinho (vinho tinto, branco ou rose): ");
+let safra = prompt("Digite a safra do seu vinho: ");
+let estoque = prompt("Digite a quantidade em estoque do seu vinho: ");
+
+alert("Cadastro realizado!");
+alert("A seguir, veja os detalhes sobre o vinho no console.");
+
+console.log("Detalhes do Vinho:");
+console.log("Nome: " + nome);
+console.log("Tipo: " + tipo);
+console.log("Safra: " + safra);
+console.log("Estoque: " + estoque);
